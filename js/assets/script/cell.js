30711d1da5a7fecd9e21c139de1a19923e6d3d7e
akriaueno <akriaueno@users.noreply.github.com>
2021-10-17 17:29:41 +0000

    deploy: d240f52d790b5aba93f8b194eaeb6e6d46ac9d75

--- js/assets/script/cell.js
+++ js/assets/script/cell.js
@@ -17,7 +17,9 @@ exports.Cell = void 0;
 var Cell = /** @class */ (function (_super) {
     __extends(Cell, _super);
     function Cell(param) {
-        var _this = _super.call(this, param) || this;
+        var _this = this;
+        console.log(param);
+        _this = _super.call(this, param) || this;
         _this.alive = g.game.random.generate() >= 0.5;
         _this.onUpdate.add(function () {
             if (_this.alive) {
